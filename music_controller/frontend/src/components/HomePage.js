059fb914cf504438dfe2305ce394d11d11095d9b
Joseph Katusabe <162023018+josephkatusabe@users.noreply.github.com>
2024-05-28 16:38:24 +0300

    I have added an ignore fie to ignore the .env
    Added styles, react components, react page routing
    using the V6 instead of Switch, use Routes and
    elements

---
+++ music_controller/frontend/src/components/HomePage.js
@@ -0,0 +1,34 @@
+import React, {Component} from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import { 
+    BrowserRouter as Router, 
+    Routes, 
+    Route, 
+    Link, 
+    Redirect 
+} from "react-router-dom";
+
+//Create a simple Home component to display the home page.
+function Home() {
+    return <p>This is the home page</p>;
+}
+
+export default class HomePage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        console.log('Component rendering')
+        return (
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/join" element={<RoomJoinPage />} />
+                    <Route path="/create" element={<CreateRoomPage />} />
+                </Routes>
+            </Router>
+        );
+    }
+}
