2938e6ffaa7a9e658a9cf8e2991ced7fc1772eaa
Joseph Katusabe <162023018+josephkatusabe@users.noreply.github.com>
2024-05-28 17:16:28 +0300

    Backend post request serializer using sessions to store data

--- music_controller/api/serializers.py
+++ music_controller/api/serializers.py
@@ -3,7 +3,13 @@
 from rest_framework import serializers
 from .models import Room
 
+#should always have an outgoing and income serializers to properly hundle the post and get requests.
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip', 'created_at')
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip')
--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,10 +1,41 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .serializers import RoomSerializer
+from rest_framework import generics, status
+from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 #these are all endpoints (a location on the webserver)
+
+#setion is a temporary connection between two computers. Already authenticated and therefore, you have to signin.
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer
+
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+        
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            guest_can_pause = serializer.data.get('guest_can_pause')
+            votes_to_skip = serializer.data.get('votes_to_skip')
+            host = self.request.session.session_key
+
+            #if user has multiple sessions, just create a new room.
+            queryset = Room.objects.filter(host=host)
+            if queryset.exists():
+                room = queryset[0]
+                room.guest_can_pause = guest_can_pause
+                room.votes_to_skip = votes_to_skip
+                room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+
+            else:
+                room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
+                room.save()
+            return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
